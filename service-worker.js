96a44dc98773bcad4f1f1a714c370e9ee10618ca
Phat <nguyenphatit.nl@gmail.com>
2018-10-18 17:12:40 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/mail/precache-manifest.ba1f0b868e23baeeeeb194a0b6da6a48.js"
+  "/mail/precache-manifest.392ff9b718591a109ce74baa8ba8ff75.js"
 );
 
 workbox.clientsClaim();
