2beb09392176e4366b5332ad3f38e6f2a63c96bb
Phat <nguyenphatit.nl@gmail.com>
2018-10-18 14:48:44 +0700

    Updates

---
+++ precache-manifest.f4753ce5fb861eec81d046e6c9f13229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mail/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8767071ea74cabc4faa1",
+    "url": "/mail/static/js/runtime~main.8767071e.js"
+  },
+  {
+    "revision": "0d6d74702ed50b7628c1",
+    "url": "/mail/static/js/main.0d6d7470.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/mail/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "0d6d74702ed50b7628c1",
+    "url": "/mail/static/css/main.0a28dd8c.chunk.css"
+  },
+  {
+    "revision": "113f06e560f15ec05350231cf9207bf7",
+    "url": "/mail/index.html"
+  }
+];
